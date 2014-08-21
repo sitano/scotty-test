dbbc288c0f4a613ccc0e348cddfe27c42c9cd332
John.Koepi <john.koepi@gmail.com>
2014-08-21 18:30:17 +0300

    SQL storage imple for URL record

--- Query.hs
+++ Query.hs
@@ -7,7 +7,7 @@ import Database.HDBC.MySQL
 import Database.HDBC
 
 data Url =
-    Url {urlId :: Integer,  -- ^ Numeric ID for this URL
+    Url {urlId :: Int,      -- ^ Numeric ID for this URL
          urlPath :: String  -- ^ URL path
         }
     deriving (Eq, Show, Read)
@@ -24,11 +24,29 @@ setup db =
           return ()
      commit db
 
-addUrl :: IConnection conn => conn -> Url -> IO Url
-addUrl db url = error "not implemented"
+addUrl :: IConnection conn => conn -> String -> IO Url
+addUrl db url =
+  handleSql err $ do
+    run db "INSERT INTO `url` (`url`) VALUES (?)" [ toSql url ]
+    r <- quickQuery' db "SELECT LAST_INSERT_ID() as `id`" []
+    case r of
+      [[x]] -> return Url {urlId = fromSql x, urlPath = url}
+      y -> fail $ "addUrl: unexpected result: " ++ show y
+    where
+      err e = fail $ "Error adding url\n" ++ show e
 
 getUrl :: IConnection conn => conn -> Int -> IO Url
-getUrl db url = error "not implemented"
+getUrl db uid =
+  do
+    r <- quickQuery' db "SELECT `url` FROM `url` WHERE `id` = ?" [ toSql uid ]
+    case r of
+      [[x]] -> return Url {urlId = uid, urlPath = fromSql x}
+      _ -> fail "no url by id"
 
 listUrl :: IConnection conn => conn -> IO [Url]
-listUrl db = error "not implemented"
+listUrl db =
+  do
+    r <- quickQuery' db "SELECT `id`, `url` FROM `url`" []
+    return (map conv r)
+  where
+    conv [x, y] = Url (fromSql x) (fromSql y)
